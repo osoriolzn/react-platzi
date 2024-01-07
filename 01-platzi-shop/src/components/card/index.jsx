04ddf8ba29b869d83ec677e68416c56418c72027
Yohan D Osorio L <yohan.osorio@arroyoconsulting.net>
2024-01-07 18:12:19 -0500

    Add fetch of data

--- 01-platzi-shop/src/components/card/index.jsx
+++ 01-platzi-shop/src/components/card/index.jsx
@@ -1,12 +1,14 @@
-function Card () {
+import PropTypes from 'prop-types'
+
+function Card ({data}) {
   return (
     <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
       <figure className='relative mb-2 w-full h-4/5'>
-        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>Electronics</span>
+        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.category.name}</span>
         <img
           className='w-full h-full object-cover rounded-lg'
-          src='https://images.pexels.com/photos/610945/pexels-photo-610945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-          alt="headphones"
+          src={data.images[0]}
+          alt={data.description}
         />
         <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'>
           +
@@ -14,11 +16,15 @@ function Card () {
       </figure>
 
       <p className='flex justify-between'>
-        <span className='text-sm font-light'>Headphones</span>
-        <span className='text-lg font-medium'>$300</span>
+        <span className='text-sm font-light'>{data.title}</span>
+        <span className='text-lg font-medium'>${data.price}</span>
       </p>
     </div>
   )
+}
+
+Card.propTypes = {
+  data: PropTypes.object
 }
 
 export default Card
--- 01-platzi-shop/src/pages/home/index.jsx
+++ 01-platzi-shop/src/pages/home/index.jsx
@@ -1,11 +1,33 @@
+import { useState, useEffect } from 'react'
 import Layout from '../../components/layout'
 import Card from '../../components/card'
 
+import responseItem from '../../mocks/with-results.json'
+// import withoutResult from '../../mocks/not-results.json'
+
+// const URL = 'https://api.escuelajs.co/api/v1/products'
+
 function Home() {
+  const [items, setItems] = useState([])
+  
+  useEffect(() => {
+    const data = responseItem
+    setItems(data)
+    // fetch(URL)
+    //   .then(response => response.json())
+    //   .then(data => setItems(data))
+  }, [])
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className='grid grid-cols-4 gap-3 w-full max-w-screen-lg'>
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
