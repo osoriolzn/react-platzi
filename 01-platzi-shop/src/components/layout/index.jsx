dea2d5b5dcaa7136e53a0334d25c31306d9a5f5f
Yohan D Osorio L <yohan.osorio@arroyoconsulting.net>
2024-01-08 20:06:25 -0500

    Add button back to top

--- 01-platzi-shop/src/components/layout/index.jsx
+++ 01-platzi-shop/src/components/layout/index.jsx
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
+import './layout.css'
 
 function Layout ({ children }) {
   return (
-    <div className='flex flex-col items-center mt-20'>
-      {children}
-    </div>
+    <>
+      <div className='flex flex-col items-center mt-20'>
+        {children}
+      </div>
+      <a id='back-top' href='#top'>Ir al inicio</a>
+    </>
   )
 }
 
