fac470f0c7dac64635cb87b7204a8c54ee52b480
Yohan Osorio <yooslzn1487@gmail.com>
2024-02-17 16:52:16 -0500

    Add component

---
+++ 02-state-management/src/ClassState.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ClassState extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Eliminar ClassState</h2>
+        <p>Escribe el código de seguridad</p>
+        <input placeholder="148632" />
+        <button>Comprobar</button>
+      </div>
+    );
+  }
+}
+
+export { ClassState }
---
+++ 02-state-management/src/UseState.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function UseState() {
+  return (
+    <div>
+      <h2>Eliminar UseState</h2>
+      <p>Escribe el código de seguridad</p>
+      <input placeholder="148632" />
+      <button>Comprobar</button>
+    </div>
+  );
+}
+
+export { UseState }
