5c99f65081ad4ed7addfa5ef59ebaa668cbab7f2
Yohan Osorio <yooslzn1487@gmail.com>
2024-02-17 17:17:20 -0500

    Add states

--- 02-state-management/src/ClassState.js
+++ 02-state-management/src/ClassState.js
@@ -1,13 +1,26 @@
 import React from "react";
 
 class ClassState extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      error: false
+    }
+  }
+
   render() {
     return (
       <div>
-        <h2>Eliminar ClassState</h2>
+        <h2>Eliminar {this.props.name}</h2>
         <p>Escribe el código de seguridad</p>
         <input placeholder="148632" />
-        <button>Comprobar</button>
+        <button onClick={() => this.setState({error: !this.state.error})}>
+          Comprobar
+        </button>
+        {this.state.error && (
+          <p>Error: Código incorrecto</p>
+        )}
       </div>
     );
   }
--- 02-state-management/src/UseState.js
+++ 02-state-management/src/UseState.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-function UseState() {
+function UseState({ name }) {
+  const [error, setError] = useState(false);
+
   return (
     <div>
-      <h2>Eliminar UseState</h2>
+      <h2>Eliminar {name}</h2>
       <p>Escribe el código de seguridad</p>
       <input placeholder="148632" />
-      <button>Comprobar</button>
+      <button onClick={() => setError(!error)}>
+        Comprobar
+      </button>
+      {error && (
+        <p>Error: Código incorrecto</p>
+      )}
     </div>
   );
 }
